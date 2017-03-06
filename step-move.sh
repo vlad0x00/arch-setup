1cf023cb66d2d1069dceef910b71557a34551cab
schutzekatze <vladimir.nik94@gmail.com>
2017-03-06 22:51:59 +0100

    Add a check to step-move.sh

--- step-move.sh
+++ step-move.sh
@@ -2,6 +2,12 @@
 
 wd=$(pwd)
 
+if [ "$(dirname $wd)" == "/home/$USERNAME" ]; then
+    echo "Already at home - skipping step"
+    chown -R $USERNAME:$USERNAME .
+    exit
+fi
+
 cp -r $wd /home/$USERNAME/
 chown -R $USERNAME:$USERNAME /home/$USERNAME/$(basename $wd)
 cd /home/$USERNAME/$(basename $wd)
