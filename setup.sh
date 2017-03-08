a4b2392ab5969254714e98fafef4e2390469a8e5
schutzekatze <vladimir.nik94@gmail.com>
2017-03-08 23:50:35 +0100

    Update

--- setup.sh
+++ setup.sh
@@ -1,79 +1,79 @@
 #!/bin/bash
 
 echo "Setting up environment variables"
-. step-env.sh
+. steps/step-env.sh
 
 echo -e "\nPress enter to continue"
 read
 
 echo "Installing xorg"
-. step-xorg.sh
+. steps/step-xorg.sh
 
 echo -e "\nPress enter to continue"
 read
 
 echo "Setting up wifi"
-. step-wifi.sh
+. steps/step-wifi.sh
 
 echo -e "\nPress enter to continue"
 read
 
 echo "Creating user"
-. step-create-user.sh
+. steps/step-create-user.sh
 
 echo -e "\nPress enter to continue"
 read
 
 echo "Moving to home"
-. step-move.sh
+. steps/step-move.sh
 
 echo -e "\nPress enter to continue"
 read
 
 echo "Installing user apps"
-. step-user-apps.sh
+. steps/step-user-apps.sh
 
 echo -e "\nPress enter to continue"
 read
 
 echo "Setting up dotfiles"
-. step-dotfiles.sh
+. steps/step-dotfiles.sh
 
 echo -e "\nPress enter to continue"
 read
 
 echo "Setting up swapfile"
-. step-swapfile.sh
+. steps/step-swapfile.sh
 
 echo -e "\nPress enter to continue"
 read
 
 echo "Installing yaourt"
-. step-yaourt.sh
+. steps/step-yaourt.sh
 
 echo -e "\nPress enter to continue"
 read
 
 echo "Installing yaourt apps"
-. step-yaourt-apps.sh
+. steps/step-yaourt-apps.sh
 
 echo -e "\nPress enter to continue"
 read
 
 echo "Setting up password for $USERNAME"
-. step-passwd.sh
+. steps/step-passwd.sh
 
 echo -e "\nPress enter to continue"
 read
 
 echo "Setting up power management"
-. step-power-management.sh
+. steps/step-power-management.sh
 
 echo -e "\nPress enter to continue"
 read
 
 echo "Deleting original dir"
-. step-delete-original.sh
+. steps/step-delete-original.sh
 
 echo -e "\nAll done"
 echo -e "\nYou should now install graphics drivers, reboot, and enjoy."
