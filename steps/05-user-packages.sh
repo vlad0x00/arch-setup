f5909222b1309cb4140ff1a79cdd413224e313ee
Vladimir Nikolić <vladimir.nik94@gmail.com>
2017-03-11 02:49:06 +0100

    OCD

--- steps/05-user-packages.sh
+++ steps/05-user-packages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Installing user apps"
+echo "Installing user packages"
 
 pacman -S qt5-base qt4 vlc pulseaudio chromium gedit eclipse-cpp geogebra htop iotop libreoffice-fresh jdk8-openjdk openjdk8-doc openjdk8-src \
 java-openjfx java-openjfx-doc java-openjfx-src gimp cowsay lolcat fortune-mod netbeans nmap openssh python php sl \
