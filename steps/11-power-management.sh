960104a6d0405daa863184cd99e2e972ee17b645
schutzekatze <vladimir.nik94@gmail.com>
2017-04-18 02:08:05 +0200

    Update

--- steps/11-power-management.sh
+++ steps/11-power-management.sh
@@ -2,18 +2,7 @@
 
 echo "Setting up power management"
 
-pacman -S powertop
-echo \
-'[Unit]
-Description=Powertop tunings
+pacman -S tlp
+systemctl enable tlp.service
 
-[Service]
-Type=oneshot
-ExecStart=/usr/bin/powertop --auto-tune
-
-[Install]
-WantedBy=multi-user.target' >/etc/systemd/system/powertop.service
-systemctl enable powertop
-
-echo "Power management by powertop has been enabled."
-echo "You should run 'powertop -c' after the installation to calibrate powertop."
+echo "TLP power management has been enabled."
