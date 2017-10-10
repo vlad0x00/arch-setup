8b27fa01d38de4508409c0f3c58c7326bb8a8894
schutzekatze <vladimir.nik94@gmail.com>
2017-10-10 11:55:16 +0200

    Remove unnecessary stuff

--- steps/01-xorg.sh
+++ steps/01-xorg.sh
@@ -2,4 +2,4 @@
 
 echo "Installing xorg"
 
-pacman -S xorg xorg-xinit xorg-server xorg-server-devel xorg-utils xorg-server-utils xorg-xrandr xorg-xinput xbindkeys mesa dmenu xcompmgr i3
+pacman -S xorg xorg-xinit xorg-server xorg-server-devel xorg-xrandr xorg-xinput xbindkeys mesa dmenu xcompmgr i3
--- steps/11-power-management.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Setting up power management"
-
-pacman -S tlp
-systemctl enable tlp.service
-
-echo "TLP power management has been enabled."
