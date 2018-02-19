c77deaaaacf3ab9651a7f510751acedb52fc46ec
schutzekatze <vladimir.nik94@gmail.com>
2018-02-19 20:14:07 +0100

    Fresh new aesthetic =]

--- steps/05-user-packages.sh
+++ steps/05-user-packages.sh
@@ -2,10 +2,10 @@
 
 echo "Installing user packages"
 
-pacman -S qt5-base qt4 vlc pulseaudio chromium gedit eclipse-cpp geogebra htop iotop libreoffice-fresh jdk8-openjdk openjdk8-doc openjdk8-src \
-java-openjfx java-openjfx-doc java-openjfx-src gimp cowsay lolcat fortune-mod nmap openssh python php sl \
-transmission-qt tree xfce4-terminal shotwell feh php noto-fonts-cjk noto-fonts-emoji neovim feh nautilus conky wget xfce4-screenshooter base-devel zsh zsh-completions \
-acpi sysstat alsa-utils
+pacman -S qt5-base qt4 vlc pulseaudio chromium gedit eclipse-cpp geogebra htop iotop jdk8-openjdk openjdk8-doc openjdk8-src \
+java-openjfx java-openjfx-doc java-openjfx-src gimp cowsay lolcat fortune-mod nmap openssh python sl \
+transmission-qt tree xfce4-terminal shotwell noto-fonts-cjk noto-fonts-emoji noto-fonts-extra ttf-font-awesome neovim nautilus conky wget xfce4-screenshooter base-devel zsh zsh-completions \
+acpi sysstat alsa-utils firefox
 
 echo "Changing default shell to zsh"
 
--- steps/09-aur-packages.sh
+++ steps/09-aur-packages.sh
@@ -5,7 +5,7 @@ echo "Installing AUR packages"
 changed_sudoers=$(cat /etc/sudoers | sed 's/.*\(%wheel ALL=(ALL) NOPASSWD: ALL\)/\1/')
 echo "$changed_sudoers" | tee /etc/sudoers >/dev/null
 
-sudo -u $USERNAME pacaur -S google-chrome ttf-google-fonts-git i3-gaps
+sudo -u $USERNAME pacaur -S google-chrome ttf-google-fonts-git i3-gaps-next-git yabar-git
 
 changed_sudoers=$(cat /etc/sudoers | sed 's/.*\(%wheel ALL=(ALL) NOPASSWD: ALL\)/# \1/')
 echo "$changed_sudoers" | tee /etc/sudoers >/dev/null
