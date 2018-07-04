50e84a446f0c542d016b19e6adf1e5a888d6caba
Vladimir Nikolić <vladimir.nik94@gmail.com>
2018-07-05 01:35:51 +0200

    Update 05-create-user.sh

--- steps/05-create-user.sh
+++ steps/05-create-user.sh
@@ -17,3 +17,12 @@ usermod -a -G wheel,audio,video $USERNAME
 echo "Setting up password"
 
 passwd $USERNAME
+
+echo "Making home dirs"
+
+cd /home/$USERNAME
+mkdir -p projects
+mkdir -p random
+mkdir -p tools/bin
+mkdir -p wallpapers
+mkdir -p temp
