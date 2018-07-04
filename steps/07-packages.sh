fb32c6c03e969f08bd781923e773528cdfccddab
Vladimir Nikolić <vladimir.nik94@gmail.com>
2018-07-05 01:23:33 +0200

    Update 07-packages.sh

--- steps/07-packages.sh
+++ steps/07-packages.sh
@@ -2,14 +2,14 @@
 
 echo "Installing user packages"
 
-pacman -S qt5-base qt4 vlc pulseaudio chromium gedit eclipse-cpp geogebra htop iotop jdk8-openjdk openjdk8-doc openjdk8-src \
-java-openjfx java-openjfx-doc java-openjfx-src gimp cowsay lolcat fortune-mod nmap openssh python sl \
-transmission-qt tree xfce4-terminal shotwell noto-fonts-cjk noto-fonts-emoji noto-fonts-extra neovim nautilus conky wget xfce4-screenshooter base-devel zsh zsh-completions \
-acpi sysstat alsa-utils firefox
+pacman -S qt5-base qt4 vlc pulseaudio gedit eclipse-cpp geogebra htop iotop jdk8-openjdk openjdk8-doc openjdk8-src \
+java-openjfx java-openjfx-doc java-openjfx-src gimp cowsay lolcat fortune-mod nmap openssh python sl keepass \
+transmission-qt tree xfce4-terminal shotwell noto-fonts-cjk noto-fonts-emoji noto-fonts-extra neovim nautilus conky wget \
+xfce4-screenshooter xclip base-devel zsh zsh-completions acpi sysstat alsa-utils chromium firefox redshift python-pywal
 
 echo "Installing AUR packages"
 
-sudo -u $USERNAME pacaur -S google-chrome ttf-google-fonts-git i3-gaps-next-git yabar-git ttf-font-awesome-4
+sudo -u $USERNAME pacaur -S google-chrome ttf-google-fonts-git i3-gaps-next-git yabar-git ttf-font-awesome-4 oh-my-zsh-git
 
 echo "Changing default shell to zsh"
 
@@ -17,4 +17,4 @@ chsh -s /usr/bin/zsh
 
 echo "Installing pip packages"
 
-pip install pywal i3ipc
+pip install i3ipc
