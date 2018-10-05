e98e7f435c5e5a400b6c4e9d9e9045b9e43f8245
Vladimir Nikolić <vladimir.nik94@gmail.com>
2018-10-06 01:51:23 +0200

    Update 05-create-user.sh

--- steps/05-create-user.sh
+++ steps/05-create-user.sh
@@ -21,7 +21,7 @@ passwd $USERNAME
 echo "Making home dirs"
 
 cd /home/$USERNAME
-mkdir -p work/projects
-mkdir -p random
-mkdir -p tools/bin
-mkdir -p wallpapers
+sudo -u $USERNAME mkdir -p work/projects
+sudo -u $USERNAME mkdir -p random
+sudo -u $USERNAME mkdir -p tools/bin
+sudo -u $USERNAME mkdir -p wallpapers
