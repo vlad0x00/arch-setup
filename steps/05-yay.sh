d4fdd888985be81edc5efadf79d16b0053663b40
Vladimir Nikolić <vladimir.nik94@gmail.com>
2018-11-15 21:20:45 +0100

    Update 05-yay.sh

--- steps/05-yay.sh
+++ steps/05-yay.sh
@@ -13,7 +13,7 @@ chown -R $USERNAME:$USERNAME . && \
 sudo -u $USERNAME makepkg -sri --skippgpcheck
 
 cd /home/$USERNAME
-rm -rf yay && rm -rf yay.tar.gz
+rm -rf yay && rm -f yay.tar.gz
 
 changed_makepkg=$(cat /etc/makepkg.conf | sed "s/PKGEXT='.pkg.tar.zx'/PKGEXT='.pkg.tar'/")
 echo "$changed_makepkg" | tee /etc/makepkg.conf >/dev/null
