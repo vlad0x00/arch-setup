3de7e030f2249d15aa8275dc5093a0b7486efea6
Vladimir Nikolić <vladimir.nik94@gmail.com>
2018-11-20 01:27:39 +0100

    Update 06-packages.sh

--- steps/06-packages.sh
+++ steps/06-packages.sh
@@ -4,7 +4,7 @@ echo "Installing user packages"
 
 pacman -S qt5-base qt4 vlc pulseaudio gedit gtop htop iotop jdk10-openjdk openjdk10-doc openjdk10-src \
 java-openjfx java-openjfx-doc java-openjfx-src gimp cowsay lolcat fortune-mod nmap openssh python python-pip sl keepass \
-tree xfce4-terminal shotwell ttf-ubuntu-font-family neovim nautilus conky wget \
+tree xfce4-terminal shotwell ttf-ubuntu-font-family neovim nautilus conky wget feh \
 xfce4-screenshooter xclip base-devel zsh zsh-completions acpi sysstat alsa-utils chromium firefox redshift python-pywal \
 xorg xorg-xinit xorg-server xorg-server-devel xorg-xrandr xorg-xinput xbindkeys mesa dmenu compton i3-gaps
 
